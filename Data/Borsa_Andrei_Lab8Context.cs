d7f75b9bbea90de6196f959504d5813df9b869e6
andre <andrey1208@yahoo.com>
2021-01-07 15:37:05 +0200

    Add project files.

---
+++ Data/Borsa_Andrei_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Borsa_Andrei_Lab8.Models;
+
+namespace Borsa_Andrei_Lab8.Data
+{
+    public class Borsa_Andrei_Lab8Context : DbContext
+    {
+        public Borsa_Andrei_Lab8Context (DbContextOptions<Borsa_Andrei_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Borsa_Andrei_Lab8.Models.Book> Book { get; set; }
+    }
+}
